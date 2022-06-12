2a2342b04efb0659d7ba956c22713068b5869ea2
Michael Okoe <michael.okoejr@gmail.com>
2022-06-12 21:43:10 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`; 
+}
+
+function mondayWork (activity = "go to the office")  {
+  return (`This Monday, I will ${activity}.`)
+}
+
+
+
+
+function wrapAdjective (result = "*") {
+  return function (inner = "handsome") {
+    return `You are ${result}${inner}${result}!`  
+      }  
+    }
+
+
